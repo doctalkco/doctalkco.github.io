d10a54a5181b33fa8ae99ca8857f7592378e06a7
Thanh "Bin" Pham <gwfreak01@gmail.com>
2020-10-29 23:15:24 -0500

    Initial Static Files

---
+++ precache-manifest.31d8b6f27d893beffce4d52004de1261.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e5e9255c7cd160f0c2bbd62987e7d1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8d9e746b8462bef610ec",
+    "url": "/my-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a96c52f42573b113af1f",
+    "url": "/my-app/static/css/main.302fc548.chunk.css"
+  },
+  {
+    "revision": "8d9e746b8462bef610ec",
+    "url": "/my-app/static/js/2.303c88bb.chunk.js"
+  },
+  {
+    "revision": "2f860bb8043bb6f3e23de11a9a1b4015",
+    "url": "/my-app/static/js/2.303c88bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96c52f42573b113af1f",
+    "url": "/my-app/static/js/main.5d7d175e.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "17891286101a134b8f31dad0e72f71e8",
+    "url": "/my-app/static/media/jotlogs_brand.17891286.svg"
+  },
+  {
+    "revision": "45213cf4976783a4d2cd312197866988",
+    "url": "/my-app/static/media/jotlogs_clay.45213cf4.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
